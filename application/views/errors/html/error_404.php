2cfa8cc22f6d200e3a495b04411fd87fd66eec6e
Chuyenbka <Chuyenbka@CHUYENPN>
2017-10-22 15:27:30 +0700

    https

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="https://crm2.lakita.vn/style/images/favicon.png" type="image/x-icon" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body style="background: url(https://lakita.vn/public/images/crm-background.jpg); background-size: cover">
         <div class="container text-center" style="margin-top: 30px; background-color:  rgba(255, 255, 255, 0.74); padding-bottom: 15px">
